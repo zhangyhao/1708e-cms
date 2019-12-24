2ba80a6297581b52af77cba54052ec8d8803919c
zhangyhao <2352996446@qq.com>
2019-12-24 08:13:55 +0800

    初始化提交

---
+++ src/main/java/com/zhangyuhao/controller/AdminController.java
@@ -0,0 +1,165 @@
+package com.zhangyuhao.controller;
+
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.hibernate.validator.constraints.pl.REGON;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
+import com.zhangyuhao.common.CmsError;
+import com.zhangyuhao.common.CmsMessage;
+import com.zhangyuhao.entity.Article;
+import com.zhangyuhao.entity.Link;
+import com.zhangyuhao.service.ArticleService;
+
+@RequestMapping("admin")
+@Controller
+public class AdminController {
+
+	@Autowired
+	ArticleService articleservice;
+	//跳转管理员页面
+	@RequestMapping("index")
+	public String index(){
+		return "admin/index";
+	}
+	/**
+	 * 文章管理
+	 * @param request
+	 * @param status
+	 * @param page
+	 * @param xl1
+	 * @param xl2
+	 * @return
+	 */
+	@RequestMapping("article")
+	public String article(HttpServletRequest request,int status,int page,Integer xl1,Integer xl2){
+		PageInfo<Article> articlePage =  articleservice.list(status,page,xl1,xl2);
+		request.setAttribute("articlePage", articlePage);
+		request.setAttribute("status", status);
+		return "admin/article/list";
+	}
+	/**
+	 * 设置热门
+	 * @param id
+	 * @param status
+	 * @return
+	 */
+	@RequestMapping("setArticeHot")
+	@ResponseBody
+	public CmsMessage setArticeHot(int id,int status){
+		/**
+		 * 数据合法性校验
+		 */
+		if(status!=0 &&status!=1){
+			
+		}
+		if(id<0){
+			
+		}
+		Article article = articleservice.getInfoById(id);
+		if(article==null){
+			
+		}
+		if(article.getStatus()==status){
+			
+		}
+		int result = articleservice.setHot(id,status);
+		if(result<1){
+			return new CmsMessage(CmsError.FAILED_UPDATE_DB, "设置失败,请重试", null);
+		}
+		return new CmsMessage(CmsError.SUCCESS,"成功",null);
+	}
+	
+	/**
+	 * 审核
+	 * @param id
+	 * @param status
+	 * @return
+	 */
+	@RequestMapping("setArticeStatus")
+	@ResponseBody
+	public CmsMessage  setArticeStatus(int id,int status) {
+		
+		/**
+		 * 数据合法性校验 
+		 */
+		if(status !=1 && status!=2) {
+			return new CmsMessage(CmsError.NOT_VALIDATED_ARGURMENT,"status参数值不合法",null);
+		}
+		
+		if(id<0) {
+			return new CmsMessage(CmsError.NOT_VALIDATED_ARGURMENT,"id参数值不合法",null);
+		}
+		
+		Article article = articleservice.getInfoById(id);
+		if(article==null) {
+			return new CmsMessage(CmsError.NOT_EXIST,"数据不存在",null);
+		}
+		
+		/**
+		 * 
+		 */
+		if(article.getStatus()==status) {
+			return new CmsMessage(CmsError.NEEDNT_UPDATE,"数据无需更改",null);
+		}
+		
+		/**
+		 *  修改数据
+		 */
+		int result = articleservice.setCheckStatus(id,status);
+		if(result<1)
+			return new CmsMessage(CmsError.FAILED_UPDATE_DB,"设置失败，请稍后再试",null);
+		
+		
+		return new CmsMessage(CmsError.SUCCESS,"成功",null);
+		
+	}
+	
+	@RequestMapping("link")
+	public Object link(HttpServletRequest request,int page){
+		PageHelper.startPage(page, 5);
+		List<Link> list = articleservice.link();
+		PageInfo<Link> pg = new PageInfo<Link>(list);
+		request.setAttribute("list", list);
+		request.setAttribute("pg", pg);
+		return "/admin/link";
+	}
+	
+	@RequestMapping("addUrl")
+	@ResponseBody
+	public Object addUrl(Link l){
+		int i = articleservice.addUrl(l);
+		return i;
+	}
+	
+	@RequestMapping("toupd")
+	@ResponseBody
+	public Object toupd(Integer id){
+		Link l = articleservice.toupd(id);
+		if(l==null){
+			return new CmsMessage(CmsError.NOT_EXIST, "用户不存在", null);
+		}
+		return new CmsMessage(CmsError.SUCCESS, "", l);
+	}
+	
+	@RequestMapping("upd")
+	@ResponseBody
+	public Object updUrl(Link l){
+		int i = articleservice.updUrl(l);
+		return i;
+	}
+	
+	@RequestMapping("del")
+	@ResponseBody
+	public Object del(Integer id){
+		int i = articleservice.del(id);
+		return i;
+	}
+}
---
+++ src/main/java/com/zhangyuhao/controller/IndexController.java
@@ -0,0 +1,167 @@
+package com.zhangyuhao.controller;
+
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.github.pagehelper.PageInfo;
+import com.zhangyuhao.entity.Article;
+import com.zhangyuhao.entity.Category;
+import com.zhangyuhao.entity.Channel;
+import com.zhangyuhao.entity.Link;
+import com.zhangyuhao.entity.Slide;
+import com.zhangyuhao.service.ArticleService;
+
+@Controller
+public class IndexController {  
+
+	@Autowired
+	ArticleService articleService;
+	/**
+	 * 首页面
+	 * @param request
+	 * @param page
+	 * @return
+	 * @throws Exception
+	 */
+	@RequestMapping(value={"index","/"})
+	public String index(HttpServletRequest request,@RequestParam(defaultValue="1")int page) throws Exception{
+		Thread t1 = new Thread(){
+			@Override
+			public void run() {
+				//获取所有的栏目
+				List<Channel> channels = articleService.getChannels();
+				request.setAttribute("channels", channels);
+			}
+		};
+		Thread t2 = new Thread(){
+			@Override
+			public void run() {
+				//获取热门文章
+				PageInfo<Article> arInfo = articleService.hotList(page);
+				request.setAttribute("articlePage", arInfo);
+			}
+		};
+		Thread t3 = new Thread(){
+			@Override
+			public void run() {
+				//获取最新文章
+				List<Article> lastArticle = articleService.lastList();
+				request.setAttribute("lastArticles", lastArticle);
+			}
+		};
+		Thread t4 = new Thread(){
+			@Override
+			public void run() {
+				List<Slide> slides = articleService.getSlides();
+				request.setAttribute("slides", slides);
+			}
+		};
+		
+		Thread t5 = new Thread(){
+			@Override
+			public void run() {
+				List<Link> list = articleService.link();
+				request.setAttribute("list", list);
+			}
+		};
+		
+		
+		
+		
+		t1.start();
+		t2.start();
+		t3.start();
+		t4.start();
+		t5.start();
+		
+		t1.join();
+		t2.join();
+		t3.join();
+		t4.join();
+		t5.join();
+		return "index";
+	}
+	/**
+	 * 点击左侧 导航
+	 * @param request  请求
+	 * @param channleId  栏目的id
+	 * @param catId 分类的id
+	 * @param page 页码
+	 * @return
+	 * @throws InterruptedException 
+	 */
+	@RequestMapping("channel")
+	public String channel(HttpServletRequest request,
+			int channelId,
+			@RequestParam(defaultValue="0") int catId,
+			@RequestParam(defaultValue="1")  int page) throws InterruptedException {
+		
+		Thread  t1 =  new Thread() {
+			public void run() {
+		// 获取所有的栏目
+		List<Channel> channels = articleService.getChannels();
+		request.setAttribute("channels", channels);
+			};
+		};
+		
+		Thread  t2 =  new Thread() {
+			public void run() {
+		// 当前栏目下  当前分类下的文章
+		PageInfo<Article> articlePage= articleService.getArticles(channelId,catId, page);
+		request.setAttribute("articlePage", articlePage);
+			};
+		};
+		
+		Thread  t3 =  new Thread() {
+			public void run() {
+		// 获取最新文章
+		List<Article> lastArticles = articleService.lastList();
+		request.setAttribute("lastArticles", lastArticles);
+			};
+		};
+		
+		Thread  t4 =  new Thread() {
+			public void run() {
+		// 轮播图
+		List<Slide> slides = articleService.getSlides();
+		request.setAttribute("slides", slides);
+		
+			};
+		};
+		
+		// 获取当前栏目下的所有的分类 catId
+		Thread  t5 =  new Thread() {
+			public void run() {
+		// 
+		List<Category> categoris= articleService.getCategoriesByChannelId(channelId);
+		request.setAttribute("categoris", categoris);
+		System.err.println("categoris is " + categoris);
+			};
+		};
+		
+		
+		t1.start();
+		t2.start();
+		t3.start();
+		t4.start();
+		t5.start();
+		
+		t1.join();
+		t2.join();
+		t3.join();
+		t4.join();
+		t5.join();
+		
+		// 参数回传
+		request.setAttribute("catId", catId);
+		request.setAttribute("channelId", channelId);
+		
+		return "channel";
+	}
+}
